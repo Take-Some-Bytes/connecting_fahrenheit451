2fb1a3eafc47b29f10a6bf7b369681797afcdb77
Take-Some-Bytes <horton0712@gmail.com>
2023-05-25 15:39:27 -0700

    Add function to manage game data

---
+++ src/gamedata.js
@@ -0,0 +1,40 @@
+/**
+ * Functions to manage game data.
+ */
+
+/**
+ * A class to represent saved game data.
+ */
+export class GameData {
+  constructor (stage, totalMistakes) {
+    /** @type {number} */
+    this.stage = stage
+    /** @type {number} */
+    this.totalMistakes = totalMistakes
+  }
+}
+
+/**
+ * Gets game data saved to local storage, if it exists.
+ * @returns {GameData|null}
+ */
+export function getGameData () {
+  const data = window.localStorage.getItem('save')
+  if (!data) {
+    return null
+  }
+
+  const parsed = JSON.parse(data)
+
+  return new GameData(parsed.stage, parsed.totalMistakes)
+}
+
+/**
+ * Saves game data to local storage.
+ * @param {GameData} data The data to save.
+ */
+export function saveGameData (data) {
+  console.log('saving game data')
+  window.localStorage.setItem('save', JSON.stringify(data))
+  window.localStorage.setItem('status', 'playing')
+}
