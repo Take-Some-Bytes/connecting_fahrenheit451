ecb87a013dd47c94f47a068d158ba2dea6211f48
Take-Some-Bytes <horton0712@gmail.com>
2023-05-25 15:38:12 -0700

    Update build chunking options

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,20 @@ export default defineConfig({
   plugins: [react()],
   build: {
     target: 'es2017',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: id => {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+          if (id.includes('scripts')) {
+            return 'scripts'
+          }
+
+          return 'index'
+        }
+      }
+    }
   }
 })
