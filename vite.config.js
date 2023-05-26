fcd3d551ed341637b1a02c1286cf649a4c9eadd9
Take-Some-Bytes <horton0712@gmail.com>
2023-05-25 19:59:30 -0700

    Make things work with subdirectory
    We're not deploying to the root, so we need to account for that.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { name } from './package.json'
+
 export default defineConfig({
+  base: `/${name}`,
   plugins: [react()],
   build: {
     target: 'es2017',
